d48f21888d87f9adeef21f6972c8f275abeec918
tszyaul6 <tszyaul6@gmail.com>
2022-04-19 23:57:21 +0800

    added try-catch for scrapper

--- backend/utils/std_article_scraper.js
+++ backend/utils/std_article_scraper.js
@@ -2,31 +2,39 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
 const std_article_scraper = async (link) => {
-	console.log(`Start scrapping article from ${link}`);
-	
-	// Launch browser and direct to the website
-	const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
-	const page = await browser.newPage();
-	await page.goto(link);
+	try {
+		console.log(`Start scrapping article from ${link}`);
 
-	// Get the html code
-	let body = await page.content();
+		// Launch browser and direct to the website
+		const browser = await puppeteer.launch({
+			executablePath: "/usr/bin/chromium-browser"
+		});
+		const page = await browser.newPage();
+		await page.goto(link);
 
-	// Close browser
-	await browser.close();
+		// Get the html code
+		let body = await page.content();
 
-	console.log(`Finished scrapping article`);
+		// Close browser
+		await browser.close();
 
-	// Let cheerio to handle the html
-	let $ = await cheerio.load(body);
+		console.log(`Finished scrapping article`);
 
-	let article = "";
-	$("article.content section div:nth-child(2) p").each((index, element) => {
-		if (!$(element).html().trim()) return;
-		article = article + "\n" + $(element).html().trim();
-	});
+		// Let cheerio to handle the html
+		let $ = await cheerio.load(body);
 
-	return article.trim();
+		let article = "";
+		$("article.content section div:nth-child(2) p").each(
+			(index, element) => {
+				if (!$(element).html().trim()) return;
+				article = article + "\n" + $(element).html().trim();
+			}
+		);
+
+		return article.trim();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = std_article_scraper;
--- backend/utils/std_info_scraper.js
+++ backend/utils/std_info_scraper.js
@@ -3,46 +3,52 @@ const cheerio = require("cheerio");
 
 // Scrap all STD news info including title, before and link in Chinese
 const std_info_scraper = async () => {
-	console.log(`Start scrapping all news info`);
+	try {
+		console.log(`Start scrapping all news info`);
 
-	// Launch browser and direct to the website
-	const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
-	const page = await browser.newPage();
-	await page.goto("https://std.stheadline.com");
+		// Launch browser and direct to the website
+		const browser = await puppeteer.launch({
+			executablePath: "/usr/bin/chromium-browser"
+		});
+		const page = await browser.newPage();
+		await page.goto("https://std.stheadline.com");
 
-	// Get the html code
-	let body = await page.content();
+		// Get the html code
+		let body = await page.content();
 
-	// Close browser
-	await browser.close();
+		// Close browser
+		await browser.close();
 
-	console.log(`Finished scrapping all news info`);
+		console.log(`Finished scrapping all news info`);
 
-	// Let cheerio to handle the html
-	let $ = await cheerio.load(body);
+		// Let cheerio to handle the html
+		let $ = await cheerio.load(body);
 
-	// Array that holds all news
-	let all_news_info = [];
+		// Array that holds all news
+		let all_news_info = [];
 
-	// Push the news title, before and link to the array as an individual object
-	$("div#instantTabPane1 div div").each((index, element) => {
-		let $2 = cheerio.load($(element).html());
-		let title = $2("div.media div:nth-child(2) h2 a").text();
-		let before = $2("div.media div:nth-child(2) span").text();
-		let link = $2("div.media div:nth-child(2) h2 a").attr("href");
-		if (!title || !link || !before) return;
+		// Push the news title, before and link to the array as an individual object
+		$("div#instantTabPane1 div div").each((index, element) => {
+			let $2 = cheerio.load($(element).html());
+			let title = $2("div.media div:nth-child(2) h2 a").text();
+			let before = $2("div.media div:nth-child(2) span").text();
+			let link = $2("div.media div:nth-child(2) h2 a").attr("href");
+			if (!title || !link || !before) return;
 
-		const this_news = {
-			title,
-			before,
-			link
-		};
+			const this_news = {
+				title,
+				before,
+				link
+			};
 
-		all_news_info.push(this_news);
-	});
+			all_news_info.push(this_news);
+		});
 
-	// Return the news array
-	return all_news_info;
+		// Return the news array
+		return all_news_info;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = std_info_scraper;
